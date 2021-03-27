ca035398645382ce4458d5718c8f20c38bf29d1f
Eliziéle Paroli <68335406+eparoli@users.noreply.github.com>
2021-03-27 17:40:42 -0300

    Add files via upload

---
+++ i18n/joia.pro
@@ -0,0 +1,7 @@
+FORMS = ../joia_dialog_base.ui
+
+SOURCES = ../joia_dialog.py \
+
+../joia.py
+
+TRANSLATIONS = joia_pt.ts
---
+++ i18n/joia_pt.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>joiaDialogBase</name>
+    <message>
+        <location filename="../joia_dialog_base.ui" line="14"/>
+        <source>Joia</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../joia_dialog_base.ui" line="42"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Plugin Joia&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../joia_dialog_base.ui" line="55"/>
+        <source>Select shapefile</source>
+        <translation>Selecionar shapefile</translation>
+    </message>
+    <message>
+        <location filename="../joia_dialog_base.ui" line="68"/>
+        <source>City name</source>
+        <translation>Cidade</translation>
+    </message>
+    <message>
+        <location filename="../joia_dialog_base.ui" line="81"/>
+        <source>Output</source>
+        <translation>Saída</translation>
+    </message>
+</context>
+</TS>
